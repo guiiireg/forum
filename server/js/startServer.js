d6c05b5f1e0726c62bb02d11640bc73ef6cc5803
Guireg Nael <guireg.nael@ynov.com>
2025-04-29 10:32:00 +0200

    [STYLE]: Refactor startServer.js for consistency and readability; update .gitignore to include .idea directory

--- server/js/startServer.js
+++ server/js/startServer.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from 'url';
-import fs from 'fs/promises';
+import { fileURLToPath } from "url";
+import fs from "fs/promises";
 
 dotenv.config();
 
@@ -11,21 +11,20 @@ const port = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const htmlDir = path.join(__dirname, '../html');
+const htmlDir = path.join(__dirname, "../html");
 
 app.use(express.static(htmlDir));
 
 async function setupRoutes() {
   try {
     const files = await fs.readdir(htmlDir);
-    
-    files.forEach(file => {
-      if (file.endsWith('.html')) {
+    files.forEach((file) => {
+      if (file.endsWith(".html")) {
         let route;
-        if (file === 'home.html') {
-            route = '/';
+        if (file === "home.html") {
+          route = "/";
         } else {
-            route = `/${file.replace('.html', '')}`;
+          route = `/${file.replace(".html", "")}`;
         }
         app.get(route, (req, res) => {
           res.sendFile(path.join(htmlDir, file));
@@ -33,11 +32,12 @@ async function setupRoutes() {
       }
     });
   } catch (error) {
+    console.error("Error while setting up routes:", error);
   }
 }
 
 setupRoutes().then(() => {
   app.listen(port, () => {
-    console.log('\x1b[36m%s\x1b[0m', `Server is running on port ${port}`);
+    console.log("\x1b[36m%s\x1b[0m", `Server is running on port ${port}`);
   });
 });
