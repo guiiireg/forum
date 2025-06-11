c0f5291f33cb68c5d185342d9b2f73986ecf4434
Guireg Nael <owozplayer@gmail.com>
2025-06-11 14:13:51 +0200

    [ADD]: privacy settings to handle RGPD

---
+++ server/js/privacy/privacy.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const lastUpdateElement = document.getElementById("last-update");
+  if (lastUpdateElement) {
+    lastUpdateElement.textContent = new Date().toLocaleDateString("fr-FR");
+  }
+}); 
